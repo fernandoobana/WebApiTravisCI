04cf3aef21b9df6775e51672d05c32d6f1b389d3
Fernando <ferzim@gmail.com>
2020-06-01 20:53:29 -0300

    commit para startar CI 1

--- WebApiTravisCI/Controllers/WeatherForecastController.cs
+++ WebApiTravisCI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApiTravisCI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "teste"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
